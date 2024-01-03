8a57fd4e26113f966a9d3462ac33a6e4024411bc
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2024-01-03 20:58:44 +0530

    add to cart function update

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-function add() {
-  console.log("hora h kuch");
+function getValue(event) {
+  var newValue = event.target.value;
+  return newValue;
+  //   console.log(event.target.value);
+}
+let allprice = [];
+function totalprice(event) {
+  let newvalue = parseInt(getValue(event));
+  allprice.push(newvalue);
+  let sum = 0;
+  allprice.forEach((num) => {
+    sum += num;
+  });
+  console.log(sum, allprice.length);
 }
