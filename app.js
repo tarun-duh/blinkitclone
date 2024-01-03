6b4d6efec4ba41a84448625d725d3860363ce711
tarun-duh <92847502+tarun-duh@users.noreply.github.com>
2024-01-03 22:21:20 +0530

    .

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-function getValue(event) {
-  var newValue = event.target.value;
-  return newValue;
-  //   console.log(event.target.value);
-}
 let allprice = [];
 function totalprice(event) {
-  let newvalue = parseInt(getValue(event));
+  let newvalue = parseInt(event.target.value);
   allprice.push(newvalue);
   let sum = 0;
   allprice.forEach((num) => {
     sum += num;
   });
-  console.log(sum, allprice.length);
+  let mycart = document.querySelector(".change");
+  mycart.textContent = `${allprice.length}items ₹${sum}`;
+  console.log(sum, allprice.length, mycart);
 }
